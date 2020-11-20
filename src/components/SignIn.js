467fac638f64d204c1b6576617698d916ee0b963
benoitfinet <benoitfinetpro@gmail.com>
2020-11-20 08:09:56 +0100

    add post form signin

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,70 +1,127 @@
-import React from 'react'
+import React, { Component } from "react";
 import './SignIn.css'
 import { Link } from 'react-router-dom';
 import logo from '../img/hookedin-logo-header.png';
 
-function SignIn() {
-    return (
-        <div>
-            <Link to="/"><img alt="logo" id="logo" src={logo} /></Link>
-            <div className="signinmain">
-                <h1>Sign In</h1>
-                <form >
-                <div>
-                    <div className="signinform">
-                    <input
-                        type="text"
-                        id="name"
-                        name="name"
-                        placeholder="Sparrow"
-                    />
-                    </div>
-                    <div className="signinform">
-                    <input
-                        type="text"
-                        id="firstname"
-                        name="firsname"
-                        placeholder="Jack"
-                    />
-                    </div>
-                    <div className="signinform">
-                    <input
-                        type="email"
-                        id="email"
-                        name="email"
-                        placeholder="pirates@hookedIn.com"
-                    />
-                    </div>
-                    <div className="signinform">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Insert password"
-                    />
-                    </div>
-                    <div className="signinform">
-                    <input
-                        type="password"
-                        id="password"
-                        name="password"
-                        placeholder="Confirm password"
-                    />
-                    </div>
-                    <div className="signinform">
-                    <Link to="/UserProfile">
-                    <input
-                    type="submit"
-                    value="Connect"
-                    id="button"
-                    />
-                    </Link>
-                    </div>
-                </div>
-                </form>
-            </div>
-        </div>
-    )
-}
+class SignIn extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        firstname: "",
+        lastname: "",
+        email : "",
+        password: "",
+        password2: "",
+        role_id: ""
+      };
+    }
+    onChange = (e) => {
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+    };
+    
+    submitForm = (e) => {
+        e.preventDefault();
+        const config = {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(this.state)
+        };
+        const url = "localhost:8000/security/register";
+        fetch(url, config)
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.error) {
+                alert(res.error);
+                } else {
+                alert('Account has been successfully created!');
+                }
+            })
+            .catch((e) => {
+                console.error(e);
+                alert("There was an error when creating the account.");
+            });
+    };
 
-export default SignIn
+    render () {
+        return (
+            <div>
+                <Link to="/"><img alt="logo" id="logo" src={logo} /></Link>
+                <div className="signinmain">
+                    <h1>Sign In</h1>
+                    <form onSubmit={this.submitForm}>
+                    <div>
+                        <div className="signinform">
+                        <input
+                            type="text"
+                            id="name"
+                            name="firstname"
+                            placeholder="Sparrow"
+                            onChange={this.onChange}
+                            value={this.state.firstname}
+                        />
+                        </div>
+                        <div className="signinform">
+                        <input
+                            type="text"
+                            id="lastname"
+                            name="firsname"
+                            placeholder="Jack"
+                            onChange={this.onChange}
+                            value={this.state.lastname}
+                        />
+                        </div>
+                        <div className="signinform">
+                        <input
+                            type="email"
+                            id="email"
+                            name="email"
+                            placeholder="pirates@hookedIn.com"
+                            onChange={this.onChange}
+                            value={this.state.email}
+                        />
+                        </div>
+                        <div className="signinform">
+                        <input
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Insert password"
+                            onChange={this.onChange}
+                            value={this.state.password}
+                        />
+                        </div>
+                        <div className="signinform">
+                        <input
+                            type="password"
+                            id="password2"
+                            name="password2"
+                            placeholder="Confirm password"
+                            onChange={this.onChange}
+                            value={this.state.password2}
+                        />
+                        </div>
+                        <div className="signinform">
+                        <select id="role_id" name="role_id">
+                            <option value="1">Recruiter</option>
+                            <option value="2"> Candidate </option>
+                        </select>
+                        </div>
+                        <div className="signinform">
+                        <input
+                        type="submit"
+                        value="Send"
+                        id="buttonSign"
+                        />
+                        </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        )
+    }
+}
+export default SignIn;
